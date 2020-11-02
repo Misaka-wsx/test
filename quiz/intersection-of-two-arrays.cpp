b48107a47a1872d8421df0aee9c303d871e5562f
Misaka-wsx <wangsixiamksh@163.com>
2020-11-02 19:25:35 +0800

    20201102

---
+++ quiz/intersection-of-two-arrays.cpp
@@ -0,0 +1,55 @@
+/*
+给定两个数组，编写一个函数来计算它们的交集。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2]
+示例 2：
+
+输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出：[9,4]
+ 
+
+说明：
+
+输出结果中的每个元素一定是唯一的。
+我们可以不考虑输出结果的顺序。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/intersection-of-two-arrays
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>result;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        nums1.erase(unique(nums1.begin(),nums1.end()),nums1.end());
+        nums2.erase(unique(nums2.begin(),nums2.end()),nums2.end());
+        for(int i=0,j=0;i<nums1.size()&&j<nums2.size();)
+        {
+            if(nums1[i]==nums2[j])
+            {
+                result.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        return result;
+    }
+};
